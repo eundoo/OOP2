12b48698b14b52cea01baaad04e434d0317148c5
DESKTOP-B6OB3TR\USER <jaksimfive@naver.com>
2021-04-07 23:20:36 +0900

    Studying Java

--- src/day2/game/Unit.java
+++ src/day2/game/Unit.java
@@ -18,13 +18,16 @@ public class Unit {
 		Life = life;
 	}
 	
+	//공격기능
 	public void attack() {
-		
+		System.out.println("상대방의 생명력을 1감소시킨다.");
 	}
+	//수비기능
 	public void defense() {
-		
+		System.out.println("상대방의 공격을 방어한다.");
 	}
+	//이동기능
 	public void move() {
-		
+		System.out.println("1칸 이동한다.");
 	}
 }
