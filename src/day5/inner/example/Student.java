46cb51f3be2a13c844edb44042bd8e2a54e72b73
DESKTOP-B6OB3TR\USER <jaksimfive@naver.com>
2021-04-12 23:19:48 +0900

    Studying Java

---
+++ src/day5/inner/example/Student.java
@@ -0,0 +1,34 @@
+package day5.inner.example;
+
+public class Student {
+
+	//private처리한 이유는 굳이 학생 정보를 보여줄 필요 없음.
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	
+	public Student(String name, int kor, int eng, int math) {
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;	
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+}
